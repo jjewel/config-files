4afb6f8083872c5eb46c55628b81aa175770fbf0
Jason Jewel <jjewe2009@gmail.com>
2018-11-09 14:56:40 +1000

    Update git-prompt added a timestamp to the prompt
    Added a timestamp and also dimmed the username and altered the colour and spacing.
    Removed New line being inserted after running commands

--- git-prompt.sh
+++ git-prompt.sh
@@ -7,14 +7,16 @@ else
 fi
 
 PS1='\[\033]0;Bash:${PWD//[^[:ascii:]]/?}\007\]' # set window title
-PS1="$PS1"'\n'                 # new line
-PS1="$PS1"'\[\033[94m\]'       # change to light blue
-#PS1="$PS1"'\u@\h '             # user@host<space>
-PS1="$PS1"'\u '             # user@host<space> (edited)
-#PS1="$PS1"'\[\033[35m\]'       # change to purple
-#PS1="$PS1"'$MSYSTEM '          # show MSYSTEM
-PS1="$PS1"'\[\033[95m\]'       # change to light magenta
-PS1="$PS1"'\w'                 # current working directory
+#PS1="$PS1"'\n'                 		 # new line
+PS1="$PS1"'\[\033[94m\]'       			 # change to light blue
+#PS1="$PS1"'\u@\h '            			 # user@host<space>
+PS1="$PS1"'\e[2m\u \]'            		 # <dim>user<space>
+PS1="$PS1"'\[\033[36m\]'      			 # change to cyan
+PS1="$PS1"'\e[22m\t '                		 # <normal>time<space>
+#PS1="$PS1"'\[\033[35m\]'      			 # change to purple
+#PS1="$PS1"'$MSYSTEM '         			 # show MSYSTEM
+PS1="$PS1"'\[\033[95m\]'      			 # change to light magenta
+PS1="$PS1"'\w'                			 # current working directory
 if test -z "$WINELOADERNOEXEC"
 then
 	GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
@@ -25,10 +27,10 @@ then
 	then
 		. "$COMPLETION_PATH/git-completion.bash"
 		. "$COMPLETION_PATH/git-prompt.sh"
-		PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-		PS1="$PS1"'`__git_ps1`'   # bash function
+		PS1="$PS1"'\[\033[36m\]' 	 # change color to cyan
+		PS1="$PS1"'`__git_ps1`'  	 # bash function
 	fi
 fi
-PS1="$PS1"'\[\033[0m\]'        # change color
-#PS1="$PS1"'\n'                 # new line
-PS1="$PS1"' $ '                 # prompt: always $
+PS1="$PS1"'\[\033[0m\]'       			 # change color
+#PS1="$PS1"'\n'                			 # new line
+PS1="$PS1"' $ '                			 # prompt: always $
